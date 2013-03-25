937e7c38cb03c0bf9c3780cd5e997e9c5c0ae47f
Mike Metral <metral@gmail.com>
2013-03-25 17:26:41 -0500

    Add pwgen to install deps

--- install_deps.sh
+++ install_deps.sh
@@ -3,7 +3,9 @@
 ssh-keygen -t rsa
 
 sudo apt-get update
-sudo apt-get install python-setuptools
+sudo apt-get install python-setuptools -y
 
 sudo easy_install pip
 sudo pip install rackspace-novaclient
+
+sudo apt-get install pwgen -y
