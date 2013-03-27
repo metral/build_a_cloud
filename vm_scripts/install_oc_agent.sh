9e65eeacb9cdaf9ecf80c04623570a29d22992a7
Mike Metral <metral@gmail.com>
2013-03-27 17:29:52 -0500

    Update OC install scripts after today's update

--- vm_scripts/install_oc_agent.sh
+++ vm_scripts/install_oc_agent.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s agent SERVER_IP PASSWORD
+#curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s agent SERVER_IP PASSWORD
+curl -s -L http://sh.opencenter.rackspace.com/install.sh | \
+       sudo bash -s - --role=agent --ip=SERVER_IP -p=PASSWORD
 
 # cleanup
 rm /etc/prep.sh
--- vm_scripts/install_oc_server.sh
+++ vm_scripts/install_oc_server.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s server 0.0.0.0 PASSWORD
-curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s dashboard
+#curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s server 0.0.0.0 PASSWORD
+#curl -L "https://bcd46edb6e5fd45555c0-409026321750f2e680f86e05ff37dd6d.ssl.cf1.rackcdn.com/install.sh" | bash -s dashboard
+curl -s -L http://sh.opencenter.rackspace.com/install.sh | \
+       sudo bash -s - --role=server -p=PASSWORD
+curl -s -L http://sh.opencenter.rackspace.com/install.sh | \
+        sudo bash -s - --role=dashboard --ip=0.0.0.0
 
 # cleanup
 rm /etc/prep.sh
