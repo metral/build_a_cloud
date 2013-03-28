b0e65c106f3489c3c62a88e9d52912b2bea1aedf
Mike Metral <metral@gmail.com>
2013-03-27 20:50:45 -0500

    Remove gitignore & chef hack scripts replaced by v3 update libvirt field

--- vm_scripts/update_chef.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-ENVIRONMENT_FILE="/usr/share/pyshared/opencenter/backends/chef-client/environment.tmpl"
-
-sed -i 's/"apply_patches": true,/"apply_patches": true,\n\t"libvirt": {\n\t\t"virt_type": "qemu"\n\t},/g' $ENVIRONMENT_FILE
-
-/etc/init.d/opencenter-agent restart
-
-rm -rf /root/*.sh
